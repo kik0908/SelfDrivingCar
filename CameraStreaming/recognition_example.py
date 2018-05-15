4eea23860a89852366d27a29da03cc9ac9268aad
Alexey <example@yandex.r>
2018-05-15 11:28:07 +0300

    basic recognition

--- CameraStreaming/recognition_example.py
+++ CameraStreaming/recognition_example.py
@@ -3,56 +3,56 @@ import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-while(True):
-   ret, frame = cap.read()
-   print (ret)
-   hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+while True:
+    ret, frame = cap.read()
 
-   hsv = cv.blur(hsv, (5, 5))
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    hsv = cv.blur(hsv, (5, 5))
 
-   lower = np.array([119, 101, 73])
-   upper = np.array([255, 255, 255])
-   thresh = cv.inRange(hsv, lower, upper)
+    lower = np.array([119, 101, 73])
+    upper = np.array([255, 255, 255])
+    thresh = cv.inRange(hsv, lower, upper)
 
-   thresh = cv.erode(thresh, None, iterations=2)
-   thresh = cv.dilate(thresh, None, iterations=4)
+    thresh = cv.erode(thresh, None, iterations=2)
+    thresh = cv.dilate(thresh, None, iterations=4)
 
-   contours = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-   contours=contours[1]
-   for cnt in contours:
-      c = sorted(contours, key=cv.contourArea, reverse=True)[0]
-      rect = cv.minAreaRect(c)
-      box = np.int0(cv.boxPoints(rect))
-      cv.drawContours(frame, [box], -1, (0, 255, 0), 3)  # draw contours in green color
-      cv.imshow("test",frame)
-      y1 = int(box[0][1])
-      x2 = int(box[1][0])
-      y2 = int(box[1][1])
-      x3 = int(box[2][0])
+    contours = cv.findContours(thresh.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours = contours[1]
+    for cnt in contours:
+        c = sorted(contours, key=cv.contourArea, reverse=True)[0]
+        rect = cv.minAreaRect(c)
+        box = np.int0(cv.boxPoints(rect))
+        cv.drawContours(frame, [box], -1, (0, 255, 0), 3)  # draw contours in green color
+        cv.imshow("test",frame)
+        y1 = int(box[0][1])
+        x2 = int(box[1][0])
+        y2 = int(box[1][1])
+        x3 = int(box[2][0])
 
-      roiImg = frame[y2:y1, x2:x3]
+        roiImg = frame[y2:y1, x2:x3]
 
-      if roiImg.any():
-         cv.imshow('roiImg', roiImg)
+        if roiImg.any():
+            cv.imshow('roiImg', roiImg)
 
-         noDrive = cv.imread("noDrive.png")
+            noDrive = cv.imread("noDrive.png")
 
-         resizedRoi = cv.resize(roiImg, (100, 100))
-         noDrive=cv.resize(noDrive,(100, 100))
+            resizedRoi = cv.resize(roiImg, (100, 100))
+            noDrive=cv.resize(noDrive,(100, 100))
 
-         xresizedRoi=cv.inRange(resizedRoi, lower, upper)
-         xnoDrive=cv.inRange(noDrive, lower, upper)
+            xresizedRoi=cv.inRange(resizedRoi, lower, upper)
+            xnoDrive=cv.inRange(noDrive, lower, upper)
 
-         identity_percent=0
-         for i in range(100):
-            for j in range(100):
-               if (xresizedRoi[i][j]==xnoDrive[i][j]):
-                  identity_percent=identity_percent+1
-         print (identity_percent)
+            identity_percent=0
+            for i in range(100):
+                for j in range(100):
+                    if (xresizedRoi[i][j]==xnoDrive[i][j]):
+                        identity_percent=identity_percent+1
+            print (identity_percent)
 
-   cv.imshow('frame', frame)
+    cv.imshow('frame', frame)
 
-   if cv.waitKey(1) & 0xFF == ord('q'):
+    if cv.waitKey(1) & 0xFF == ord('q'):
        break
+
 cap.release()
 cv.destroyAllWindows()
